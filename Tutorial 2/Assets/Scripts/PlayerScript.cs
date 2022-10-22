b5e542445ef7119fb3a72b1c7917d5c64aad04a4
unknown <MonadoNobody@gmail.com>
2022-10-22 15:43:53 -0400

    Challenge Commit, I am death destroyer of Worlds

--- Tutorial 2/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -9,13 +10,26 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
-    
+    public GameObject winTextObject;
+    public TextMeshProUGUI winText;
+    Animator anim;
+    public Text lives;
+    private int lifeValue = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClip;
+    public AudioClip winClip;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = lifeValue.ToString();
+        winTextObject.SetActive(false);
+        anim = GetComponent<Animator>();
+        musicSource.clip = musicClip;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
     // Update is called once per frame
@@ -26,7 +40,16 @@ public class PlayerScript : MonoBehaviour
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
 
-        
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -0.5f;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 0.5f;
+        }
+        transform.localScale = characterScale;
 
     }
     
@@ -37,6 +60,30 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+        {
+            transform.position = new Vector3(50.0f, 0.5f, 0.0f);
+            lifeValue = 3;
+            lives.text = lifeValue.ToString();
+        }
+        if (scoreValue == 8)
+        {
+            winTextObject.SetActive(true);
+            musicSource.clip = winClip;
+            musicSource.Play();
+            musicSource.loop = false;
+        }
+        }
+        
+        if(collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            lifeValue = lifeValue - 1;
+            lives.text = lifeValue.ToString();
+            if(lifeValue == 0)
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 
@@ -44,10 +91,23 @@ public class PlayerScript : MonoBehaviour
     {
         if(collision.collider.tag == "Ground")
         {
+            anim.SetInteger("State", 0);
+            if(Input.GetKey(KeyCode.D))
+            {
+                anim.SetInteger("State", 2);
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                anim.SetInteger("State", 2);
+            }
             if(Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 2), ForceMode2D.Impulse);
+                anim.SetInteger("State", 1);
             }
+
         }
     }
+
+    
 }
